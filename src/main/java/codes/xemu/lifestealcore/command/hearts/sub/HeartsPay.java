e8faf4da06bf39e13309f23d61016e095574342b
Magnus K <74688700+codebyxemu@users.noreply.github.com>
2022-11-02 10:30:37 +0100

    Preparing for rewrite

---
+++ src/main/java/codes/xemu/lifestealcore/command/hearts/sub/HeartsPay.java
@@ -0,0 +1,15 @@
+package codes.xemu.lifestealcore.command.hearts.sub;
+
+import codes.xemu.lifestealcore.command.SubCommand;
+import org.bukkit.entity.Player;
+
+public class HeartsPay implements SubCommand {
+	@Override
+	public void execute(Player player) {
+
+	}
+
+	public void execute(Player player, String[] args) {
+
+	}
+}
